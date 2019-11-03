97c6c2faeab59d56b7602fe0bc2b4ea2b61b0a45
edling <joel.edling1@gmail.com>
2019-11-03 19:10:01 +0100

    entity added for Find Us

---
+++ projekt-liv - Copy/src/assets/data/FindUsEntity.ts
@@ -0,0 +1,16 @@
+import { FindUsTypeModel } from '@/types/FindUsTypeModel.ts';
+
+const findUsTypeData: FindUsTypeModel[] = [
+  {
+    Name: 'KFUM',
+    Id: 1,
+    Street: 'KFUM Betaniaplan 4 Vån 1',
+    City: 'Malmö',
+    Phone: '+46 (0) 707 28 56 62',
+    Email: 'Info@meraliv.se',
+    Url: 'https://maps.google.com/maps?q=BETIANIAPLAN%204&t=&z=13&ie=UTF8&iwloc=&output=embed',
+  },
+];
+const findUsType: FindUsTypeModel[] = findUsTypeData;
+
+export default findUsType;
